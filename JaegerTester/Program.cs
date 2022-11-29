87eca19a931c46aa869c4b1de4eb77a5d131f0a5
Mohammad Hossein Karami <mhkarami97@gmail.com>
2022-11-29 12:26:55 +0330

    init

---
+++ JaegerTester/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace JaegerTester
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Start");
+
+                Console.WriteLine("Please Enter jaegerAgentHost ex:(172.20.19.68 or st-dt-tools) : ");
+                var jaegerAgentHost = Console.ReadLine();
+
+                Console.WriteLine("Please Enter jaegerAgentPort ex:(31392) : ");
+                var jaegerAgentPort = Console.ReadLine();
+
+                TraceLogger.InitOpenTelemetry("JaegerTester", jaegerAgentHost, Convert.ToInt32(jaegerAgentPort));
+
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Start First Trace");
+                using (new TraceLogger("TestAction"))
+                {
+                    Console.WriteLine("Wait 500 MilliSecond");
+                    Thread.Sleep(500);
+                    Console.WriteLine("FinishWait");
+                    Console.WriteLine("----------");
+                    Console.WriteLine("----------");
+                }
+                Console.WriteLine("End First Trace");
+                Console.WriteLine("-----------------------------");
+                
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Start Second Trace");
+                using (new TraceLogger("TestAction"))
+                {
+                    Console.WriteLine("Wait 300 MilliSecond");
+                    Thread.Sleep(300);
+                    Console.WriteLine("FinishWait");
+                    Console.WriteLine("----------");
+                    Console.WriteLine("----------");
+                }
+                Console.WriteLine("End Second Trace");
+                Console.WriteLine("-----------------------------");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
---
+++ JaegerTester/TraceLogger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace JaegerTester
+{
+    public sealed class TraceLogger : IDisposable
+    {
+        private Activity _activity;
+        public static readonly ActivitySource ActivitySource = new ActivitySource("Jaeger.Test");
+
+        public TraceLogger(string actionName)
+        {
+            _activity = ActivitySource.StartActivity(actionName);
+        }
+
+        public void Dispose()
+        {
+            _activity?.Dispose();
+        }
+
+        public static void InitOpenTelemetry(string serviceName, string jaegerAgentHost, int jaegerAgentPort, Func<TracerProviderBuilder, TracerProviderBuilder> custom = null)
+        {
+            try
+            {
+                var sdk = Sdk.CreateTracerProviderBuilder()
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
+                    .AddSource("Jaeger.Test")
+                    .AddAspNetInstrumentation();
+
+                if (custom != null)
+                {
+                    sdk = custom(sdk);
+                }
+
+                sdk.AddJaegerExporter(options =>
+                {
+                    options.AgentHost = jaegerAgentHost;
+                    options.AgentPort = jaegerAgentPort;
+                });
+
+                sdk.AddConsoleExporter();
+
+                sdk.Build();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
